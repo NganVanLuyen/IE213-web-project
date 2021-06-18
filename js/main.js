e18b460f4d822f2f2632f9380c8a746b2a893e40
NganVanLuyen <nganvanluyen@gmail.com>
2021-06-18 10:59:23 +0700

    adjust post

--- js/main.js
+++ js/main.js
@@ -10,12 +10,42 @@ $('.slider-one')
    });
 
 // catagory slide
-   $('.slider-two')
+$('.slider-two')
    .not(".slick-inialized")
    .slick({
       prevArrow: ".site-slider-two .prev",
       nextArrow: ".site-slider-two .next",
       slidesToShow: 5,
-      slidesToScroll:1,
-      autoplaySpeed: 3000
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      responsive: [
+         {
+            breakpoint: 1024,
+            settings: {
+               slidesToShow: 4,
+               slidesToScroll: 1,
+               prevArrow: ".site-slider-two .prev",
+               nextArrow: ".site-slider-two .next",
+            }
+         },
+         {
+            breakpoint: 800,
+            settings: {
+               slidesToShow: 3,
+               slidesToScroll: 1,
+               prevArrow: ".site-slider-two .prev",
+               nextArrow: ".site-slider-two .next",
+            }
+         },
+         {
+            breakpoint: 600,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 1,
+               prevArrow: ".site-slider-two .prev",
+               nextArrow: ".site-slider-two .next",
+            }
+         }
+      ]
    });
